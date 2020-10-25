4c8f6b7cf2bf4334202d70b418cf599d8ca002b8
Giacomo Longo <gabibbo97@gmail.com>
2020-10-25 17:09:55 +0100

    Include scripts in release package

--- seminar-scripts/autobuild-package.sh
+++ seminar-scripts/autobuild-package.sh
@@ -8,5 +8,5 @@ tar \
   --verbose \
   --directory=output \
   --zstd \
-  --file output/buildroot.tar.zst ./bootable-rootfs ./cross-compiler ./rootfs
+  --file output/buildroot.tar.zst ./bootable-rootfs ./cross-compiler ./rootfs ./boot-rootfs.sh ./run-cross-gdb.sh
 (cd output && sha512sum buildroot.tar.zst > buildroot.tar.zst.sha512sums)
